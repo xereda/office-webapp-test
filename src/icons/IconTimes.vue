e91ae16a6d63767bb700878eac297acb7fa5a4ce
Jackson Ricardo Schroeder <xereda@Gis-Mac-mini.local>
2021-02-18 08:49:29 -0300

    feat(icons): add icons based vue sfc

---
+++ src/icons/IconTimes.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    class="w-7 h-7 mr-8 text-gray-400 z-10 cursor-pointer"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    @click="$emit('close')"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M6 18L18 6M6 6l12 12"
+    />
+  </svg>
+</template>
