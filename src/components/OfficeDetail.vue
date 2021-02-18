21df00675569c6a87124a7021b6a8c1fb08322b8
Jackson Ricardo Schroeder <xereda@Gis-Mac-mini.local>
2021-02-18 17:50:31 -0300

    feat(style): add style to footer card and add new icons for edit e remove action

--- src/components/OfficeDetail.vue
+++ src/components/OfficeDetail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-gray-600 bg-white pt-7 px-7 leading-9 font-light">
+  <div class="text-gray-600 bg-white p-7 leading-9 font-light">
     <strong class="text-xl font-semibold">
       {{ fullName }}
     </strong>
@@ -12,16 +12,36 @@
     <p>
       {{ phone }}
     </p>
-    <div class="flex justify-between border-t mt-4">
-      <button class="p-3" @click="$emit('edit', id)">EDIT</button>
-      <button class="p-3" @click="$emit('remove', id)">DELETE</button>
+    <div
+      class="flex justify-between items-center border-t pt-4 mt-4 text-gray-300"
+    >
+      <div class="flex items-center cursor-pointer" @click="$emit('edit', id)">
+        <div>
+          <svg-icon class="w-7 h-7" icon="pencil" />
+        </div>
+        <div class="text-xs ml-2">EDIT</div>
+      </div>
+      <div
+        class="flex items-center cursor-pointer text-red-300"
+        @click="$emit('remove', id)"
+      >
+        <div>
+          <svg-icon class="w-7 h-7" icon="trash" />
+        </div>
+        <div class="text-xs ml-2">DELETE</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import SvgIcon from '@/components/SvgIcon.vue';
+
 export default {
   name: 'OfficeDetail',
+  components: {
+    SvgIcon,
+  },
   props: {
     id: {
       type: String,
