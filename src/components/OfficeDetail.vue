ea78eb0804bff1abc1136f02676e6c5b595b76fb
Jackson Ricardo Schroeder <xereda@Gis-Mac-mini.local>
2021-02-20 15:09:52 -0300

    feat(component): add simple button component for footer card

--- src/components/OfficeDetail.vue
+++ src/components/OfficeDetail.vue
@@ -12,37 +12,26 @@
     <p>
       {{ phone }}
     </p>
-    <div
-      class="flex justify-between items-center border-t pt-4 mt-4 text-gray-300"
-    >
-      <div class="flex items-center cursor-pointer" @click="$emit('edit', id)">
-        <div>
-          <svg-icon class="w-7 h-7" icon="pencil" />
-        </div>
-        <div class="text-xs ml-2">EDIT</div>
-      </div>
-      <div
-        class="flex items-center cursor-pointer text-red-300"
+    <div class="flex justify-between items-center border-t pt-4 mt-4">
+      <simple-button icon="pencil" label="EDIT" @click="$emit('edit', id)" />
+      <simple-button
+        icon="trash"
+        label="DELETE"
+        class="text-red-300"
+        :is-loading="isRemoving"
         @click="$emit('remove', id)"
-      >
-        <div v-if="!isRemoving">
-          <svg-icon class="w-7 h-7" icon="trash" />
-        </div>
-        <div class="text-xs ml-2">
-          {{ isRemoving ? 'Removing office... ' : 'DELETE' }}
-        </div>
-      </div>
+      />
     </div>
   </div>
 </template>
 
 <script>
-import SvgIcon from '@/components/SvgIcon.vue';
+import SimpleButton from '@/components/SimpleButton.vue';
 
 export default {
   name: 'OfficeDetail',
   components: {
-    SvgIcon,
+    SimpleButton,
   },
   props: {
     id: {
---
+++ src/components/SimpleButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <button
+    class="inline-flex items-center text-gray-400 focus:outline-none"
+    @click="$emit('click')"
+  >
+    <svg
+      v-if="isLoading"
+      class="animate-spin h-7 w-7"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        class="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        stroke-width="4"
+      ></circle>
+      <path
+        class="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
+    <svg-icon v-else v-bind="{ icon }" class="w-7 h-7" />
+    <span class="pl-2 text-xs font-light">{{ label }}</span>
+  </button>
+</template>
+
+<script>
+import SvgIcon from '@/components/SvgIcon.vue';
+
+export default {
+  name: 'SimpleButton',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  components: {
+    SvgIcon,
+  },
+  emits: ['click'],
+};
+</script>
